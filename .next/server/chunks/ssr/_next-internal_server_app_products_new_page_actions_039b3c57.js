1a18005a51e6471c4458d296984c339091b2e0ef
jair-angel777 <1636763@senati.pe>
2026-03-06 14:35:07 -0500

    chore: eliminar .next del repositorio y actualizar .gitignore

--- .next/server/chunks/ssr/_next-internal_server_app_products_new_page_actions_039b3c57.js
+++
@@ -1,3 +0,0 @@
-module.exports=[57894,a=>{"use strict";a.s([])}];
-
-//# sourceMappingURL=_next-internal_server_app_products_new_page_actions_039b3c57.js.map
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"kKi5u+N45QbxpjICjkUSiqgcKMrAbXRZaPGNvHV/cAg=\"\n}"
